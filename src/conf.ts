e548eeb92577f49fbd0d668bffad2f3f6b59fd5c
Nikolay Khvilon <n@khvilon.ru>
2022-11-21 23:53:02 +0300

    Git ignore front conf

--- src/conf.ts
+++
@@ -1,6 +0,0 @@
-export default class conf {
-  static base_url = "http://localhost:3001/";
-  //static base_url = "http://unkaos.online:3001/";
-  //static base_url = "https://unkaos.oboz.tech:3002/";
- 
-}
