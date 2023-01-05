859e8a0663809c21b5839eb3e1f9848e1879dd05
Khvilon Nikolay <n@khvilon.ru>
2023-01-05 15:57:52 +0300

    Finished Cerberus service for the big refactoring

---
+++ server/cerberus/sql.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const postgres = require('postgres');
+const tools = require('../tools');
+const db_conf_json_1 = __importDefault(require("../db_conf.json"));
+let cerberusDbConf = tools.obj_clone(db_conf_json_1.default);
+cerberusDbConf.publications = 'cerberus_publication';
+const sql = postgres(cerberusDbConf);
+exports.default = sql;
