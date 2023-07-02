242b5370da9f2116c79446dc7f4deb00cbfd41cf
Nikolay Khvilon <n@khvilon.ru>
2023-07-02 22:13:46 +0300

    Add conf for front from gitignore

--- src/conf.ts
+++ src/conf.ts
@@ -4,7 +4,7 @@ export default class conf {
   //static base_url = "https://unkaos.oboz.tech:3002/";
   //static wsUrl = "wss://unkaos.oboz.tech:3003/";
   static base_url = process.env.BASE_URL || "https://unkaos.oboz.tech:3002/"
-  static wsUrl = process.env.WS_URL ||  "wss://unkaos.oboz.tech:3003/"
+  static wsUrl = process.env.WS_URL || "wss://unkaos.oboz.tech:3003/"
 
  
 }
