1d3880da862b7dadf9667571eb65a54d0b8a91b3
Nikolay Khvilon <n@khvilon.ru>
2023-08-14 20:01:19 +0300

    Cerberus duplicate code fix

--- server/cerberus/sql.ts
+++ server/cerberus/sql.ts
@@ -15,20 +15,7 @@ try {
   };
 }
 
-let dbConf: any;
 
-try {
-  const dbConfFile = require('../db_conf.json');
-  dbConf = dbConfFile;
-} catch (error) {
-  dbConf = {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-  };
-}
 console.log('dbConf', dbConf)
 let cerberusDbConf = tools.obj_clone(dbConf)
 cerberusDbConf.publications = 'cerberus_publication'
