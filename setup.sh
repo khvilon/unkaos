1268552d1cff6751f873a90d3e298823f5dc4280
Nikolay Khvilon <n@khvilon.ru>
2023-08-18 17:42:45 +0300

    Debug setup

--- setup.sh
+++ setup.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
-echo "Starting setup script..."
+# Path to the .env file
+ENV_FILE=".env"
+TEMP_FILE=".env.tmp"
 
-# Temporary array to store modified lines
-declare -a modified_lines=()
+# Create a temporary file to store the new values
+touch $TEMP_FILE
 
-# 1. Prompt user to set each variable in .env (or skip)
-while IFS= read -r line; do
-    echo "Reading line: $line"
-    if [[ $line != \#* && $line = *'='* ]]; then
-        var_name="${line%=*}"
-        current_value="${line#*=}"
-        echo "Prompting for $var_name..."
-        read -p "Set value for $var_name (current: $current_value) or press enter to skip: " new_value
-        if [ ! -z "$new_value" ]; then
-            line="$var_name=$new_value"
+# Read the .env file line by line
+while IFS= read -r line
+do
+    # Check if the line contains a key-value pair
+    if [[ $line == *=* ]]; then
+        key=$(echo $line | cut -d'=' -f1)
+        value=$(echo $line | cut -d'=' -f2)
+
+        # Prompt the user to change the value or skip
+        read -p "$key [$value]: " new_value
+
+        # If the user entered a new value, use it; otherwise, use the original value
+        if [[ -z $new_value ]]; then
+            echo "$key=$value" >> $TEMP_FILE
+        else
+            echo "$key=$new_value" >> $TEMP_FILE
         fi
+    else
+        # If the line doesn't contain a key-value pair, just copy it to the temporary file
+        echo "$line" >> $TEMP_FILE
     fi
-    modified_lines+=("$line")
-done < /var/app/unkaos/.env
+done < $ENV_FILE
 
-# Write modified content back to .env
-printf "%s\n" "${modified_lines[@]}" > /var/app/unkaos/.env
+# Replace the original .env file with the updated values
+mv $TEMP_FILE $ENV_FILE
 
-
+echo "Updated .env file saved."
 
 
 # 2. Make a copy of the server/db/public.sql with the changed schema
