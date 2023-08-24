7175a5fc7e42a0cc1798ffd6306876470b4342f5
Nikolay Khvilon <n@khvilon.ru>
2023-08-24 14:10:53 +0300

    Fix setup frontend

--- www/src/conf.ts
+++ www/src/conf.ts
@@ -1,8 +1,11 @@
 let base_url: any = import.meta.env.BASE_URL; 
 let wsUrl: any = import.meta.env.WS_URL;
 
-if(!base_url || (base_url.length < 3)) base_url = "https://unkaos.oboz.tech:3002/";
-if(!wsUrl || (wsUrl.length < 3)) wsUrl = "wss://unkaos.oboz.tech:3003/";
+//if(!base_url || (base_url.length < 3)) base_url = "https://unkaos.oboz.tech:3002/";
+//if(!wsUrl || (wsUrl.length < 3)) wsUrl = "wss://unkaos.oboz.tech:3003/";
+
+base_url = base_url.replace ('https://', window.location.href.split('.')[0])
+wsUrl = wsUrl.replace ('wss://',  window.location.href.split('.')[0].replace('https://', 'wss://'))
 
 console.log('>>>>>>>>>>>base_url', base_url)
 
@@ -14,3 +17,4 @@ export default class conf {
   static base_url = base_url;
   static wsUrl = wsUrl; 
 }
+
