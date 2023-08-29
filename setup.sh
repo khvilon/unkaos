d5e44b0137cfeada782d33c10a0637cce8a793db
Nikolay Khvilon <n@khvilon.ru>
2023-08-29 16:21:36 +0300

    Upgrade setup ssl

--- setup.sh
+++ setup.sh
@@ -61,26 +61,7 @@ sed -i "s/\btest\b/$schema_name/g" server/db/workspace.sql
 random_password=$(generate_password)
 sed -i "s/mypass/$random_password/g" server/db/workspace.sql
 
-# 4. Setting up SSL with Certbot if user have no sertificate
-read -p "Do you want to use Certbot for SSL setup? (yes by default, press ENTER) or type 'no' to skip: " use_certbot < /dev/tty
-use_certbot=${use_certbot:-yes}  # Set default value
-if [[ $use_certbot != "no" ]]; then
-    # Install Certbot
-    sudo apt install certbot -y
-
-   # Generate certificates
-    source $ENV_FILE
-    sudo certbot certonly --standalone -d $DOMAIN -d *.$DOMAIN --register-unsafely-without-email --agree-tos --no-eff-email
-
-    echo "try install cert 1"
-
-    cp -rfL /etc/letsencrypt/live/$DOMAIN/* /var/app/unkaos/nginx/ssl
-
-    # Create a Cron Job for auto renewal of certificates
-    (crontab -l ; echo "0 */12 * * * /usr/bin/certbot renew --quiet --post-hook \"docker-compose -f /var/app/unkaos/docker-compose.yml restart nginx\"") | crontab -
-fi
-
-# 5. Run docker conainers
+# 4. Run docker conainers
 apt install docker-compose -y
 docker-compose up -d
 
