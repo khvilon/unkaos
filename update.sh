e965673edaf913a7b1ad12680d1a6b7f60b9d0c6
Nikolay Khvilon <n@khvilon.ru>
2023-09-05 20:08:24 +0300

    Continue work on update.sh

--- update.sh
+++ update.sh
@@ -8,10 +8,6 @@ AUTO_UPDATE=true
 META_FILE_URL="https://raw.githubusercontent.com/khvilon/unkaos/dev/meta.json"
 YML_PATH="/var/docker-compose.yml"
 
-# Utility Functions
-read_current_version() {
-  CURRENT_VERSION=$(jq -r '.version' /var/meta.json)
-}
 
 is_time_allowed() {
   CURRENT_TIME=$(date +"%H:%M")
@@ -22,12 +18,27 @@ is_time_allowed() {
   fi
 }
 
+if [[ "$AUTO_UPDATE" != true || ! is_time_allowed ]]; then
+  echo "Exiting due to AUTO_UPDATE being false or update time not allowed."
+  exit 0
+fi
+
+CURRENT_VERSION=$(grep -o '"version": "[^"]*' /var/app/unkaos/meta.json | grep -o '[0-9].*')
+
+
+NEW_VERSION=$(curl -s $META_FILE_URL | grep -o '"version": "[^"]*' | grep -o '[0-9].*')
+if [[ "$NEW_VERSION" != "$CURRENT_VERSION" ]]; then
+  echo "New version $NEW_VERSION available."
+else
+  echo "Your version is up to date."
+fi
+
 
 # Main Script
 echo "Autoupdate conf: $AUTO_UPDATE, $ALLOWED_UPDATE_FROM-$ALLOWED_UPDATE_TO, $CHECK_INTERVAL"
 
-read_current_version
 
 echo $CURRENT_VERSION
+echo $CURRENT_TIME
 
 
