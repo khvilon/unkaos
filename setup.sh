c99df0d5ef7778ec6b68cd3ba024d72a01e75732
Nikolay Khvilon <n@khvilon.ru>
2023-09-08 18:38:11 +0300

    Fix setup

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ generate_password() {
     cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1
 }
 
-# 1. clone the repo
+# 1. Clone the repo
 echo "127.0.0.1 $(hostname)" | sudo tee -a /etc/hosts
 sudo apt update
 mkdir /var/app
@@ -14,71 +14,39 @@ sudo apt install git -y
 git clone -b dev https://github.com/khvilon/unkaos.git
 cd /var/app/unkaos
 
-# 2. set your env variables
+# 2. Set your env variables
 # Path to the .env file
 ENV_FILE=".env"
 TEMP_FILE=".env.tmp"
-ENV_TO_CHANGE="env_to_change"  # Specify the file containing variables to change
 
 # Create a temporary file to store the new values
 touch $TEMP_FILE
 
-# Read the env_to_change file line by line
+# Read the .env file line by line
 while IFS= read -r line
 do
-    # Check if the line contains a variable name and comment
-    if [[ $line =~ ^#.*=.* ]]; then
-        # Extract the variable name and comment
-        var_and_comment=($line)
-        var_name="${var_and_comment[0]#*#}"
-        comment="${var_and_comment[@]:1}"
+    # Check if the line contains a key-value pair
+    if [[ $line == *=* ]]; then
+        key=$(echo $line | cut -d'=' -f1)
+        value=$(echo $line | cut -d'=' -f2)
 
-        # Get the current value from .env
-        current_value=$(grep -oP "(?<=^$var_name=).*" $ENV_FILE)
+        # Prompt the user to change the value if it's preceded by a comment line beginning with #>>
+        if grep -q "^#>>$key=" $ENV_FILE; then
+            echo "Current value for $key is: $value"
+            read -p "Enter a new value or press ENTER to keep the current value: " new_value < /dev/tty
 
-        # Check if it's the DB_PASSWORD variable
-        if [[ "$var_name" == "DB_PASSWORD" ]]; then
-            # Prompt the user to change the value or generate a random password
-            echo "Current value for $var_name ($comment) is: $current_value"
-            read -p "Enter a new value or press ENTER to generate a random password: " new_value < /dev/tty
-
+            # If the user entered a new value, use it; otherwise, use the original value
             if [[ -z $new_value ]]; then
-                new_value=$(generate_password)
-                echo "Generated random password: $new_value"
+                echo "$key=$value" >> $TEMP_FILE
+            else
+                echo "$key=$new_value" >> $TEMP_FILE
             fi
         else
-            # Prompt the user to change the value or skip
-            echo "Current value for $var_name ($comment) is: $current_value"
-            read -p "Enter a new value or press ENTER to keep the current value: " new_value < /dev/tty
-        fi
-
-        # If the user entered a new value, use it; otherwise, use the original value
-        if [[ -z $new_value ]]; then
-            echo "$var_name=$current_value" >> $TEMP_FILE
-        else
-            echo "$var_name=$new_value" >> $TEMP_FILE
-        fi
-    else
-        # If the line doesn't contain a variable name and comment, just copy it to the temporary file
-        echo "$line" >> $TEMP_FILE
-    fi
-done < $ENV_TO_CHANGE
-
-# Append variables from the original .env file that should not be changed
-while IFS= read -r line
-do
-    # Check if the line contains a variable name and value
-    if [[ $line == *=* ]]; then
-        # Extract the variable name from the line
-        var_name=$(echo "$line" | cut -d'=' -f1)
-        
-        # Check if this variable is not in the env_to_change file
-        if ! grep -q "^$var_name=" $ENV_TO_CHANGE; then
-            # If not in env_to_change, copy it to the temporary file
+            # If not preceded by #>>, keep the original line
             echo "$line" >> $TEMP_FILE
         fi
     else
-        # If the line doesn't contain a variable name and value, just copy it to the temporary file
+        # If the line doesn't contain a key-value pair, just copy it to the temporary file
         echo "$line" >> $TEMP_FILE
     fi
 done < $ENV_FILE
@@ -96,7 +64,7 @@ sed -i "s/\btest\b/$schema_name/g" server/db/_workspace.sql
 random_password=$(generate_password)
 sed -i "s/mypass/$random_password/g" server/db/_workspace.sql
 
-# 4. Setting up SSL with Certbot if user has no certificate
+# 4. Setting up SSL with Certbot if the user has no certificate
 read -p "Do you want to use Certbot for SSL setup? (yes by default, press ENTER) or type 'no' to skip: " use_certbot < /dev/tty
 use_certbot=${use_certbot:-yes}  # Set default value
 if [[ $use_certbot != "no" ]]; then
@@ -113,7 +81,7 @@ if [[ $use_certbot != "no" ]]; then
 
     cp -rfL /etc/letsencrypt/live/$DOMAIN/* /var/app/unkaos/nginx/ssl
 
-    # Create a Cron Job for auto renewal of certificates
+    # Create a Cron Job for auto-renewal of certificates
     (crontab -l ; echo "0 */12 * * * /usr/bin/certbot renew --quiet --post-hook \"docker-compose -f /var/app/unkaos/docker-compose.yml restart nginx\"") | crontab -
 fi
 
@@ -121,7 +89,7 @@ fi
 apt install docker-compose -y
 docker-compose up -d
 
-# apt install postgresql-client-common
+#apt install postgresql-client-common
 sudo apt-get install postgresql-client
 
 echo "Setup complete!"
