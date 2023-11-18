dff9c92e9a4fbc13e33c89559d292b35b9df22ea
Khvilon Nikolay <n@khvilon.ru>
2023-11-18 14:17:51 +0300

    Update node to 18 in docker

--- www/src/conf.ts
+++ www/src/conf.ts
@@ -2,8 +2,8 @@ let base_url = window.location.protocol + '//' + window.location.hostname + ':30
 let wsUrl = 'wss://' + window.location.hostname + ':3003/'
 
 if(window.location.hostname == 'unkaos.local'){
-  base_url = "https://unkaos.online:3002/";
-  wsUrl = "wss://unkaos.online:3003/";
+  base_url = "https://unkaos.ru:3002/";
+  wsUrl = "wss://unkaos.ru:3003/";
 }
 
 export default class conf {
