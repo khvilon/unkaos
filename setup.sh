e944bd654bad78fdc75c10ab3703c8bd81c3ae85
Khvilon Nikolay <n@khvilon.ru>
2023-11-21 15:28:24 +0300

    Update setup script for multi linux use

--- setup.sh
+++ setup.sh
@@ -5,12 +5,57 @@ generate_password() {
     cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1
 }
 
+# Function to validate the workspace name
+validate_workspace_name() {
+    local name=$1
+    if [[ -z $name ]]; then
+        echo "Workspace name cannot be empty."
+        return 1
+    elif [[ $name =~ [^a-zA-Z0-9_] ]]; then
+        echo "Workspace name can only contain letters, numbers, and underscores."
+        return 1
+    elif [[ ${#name} -ge 64 ]]; then
+        echo "Workspace name should be shorter than 64 characters."
+        return 1
+    elif [[ ! $name =~ ^[a-zA-Z_] ]]; then
+        echo "Workspace name should start with a letter or an underscore."
+        return 1
+    fi
+    return 0
+}
+
+# Detect OS and set package manager commands
+OS=$(awk -F= '/^ID=/{print $2}' /etc/os-release)
+PKG_UPDATE=""
+PKG_INSTALL=""
+
+case $OS in
+    ubuntu|debian|raspbian)
+        PKG_MANAGER="apt"
+        PKG_UPDATE="$PKG_MANAGER update"
+        PKG_INSTALL="$PKG_MANAGER install -y"
+        CERT_INSTALL="$PKG_INSTALL certbot"
+        DOCKER_INSTALL="$PKG_INSTALL docker.io docker-compose"
+        ;;
+    centos)
+        PKG_MANAGER="yum"
+        PKG_UPDATE="$PKG_MANAGER update"
+        PKG_INSTALL="$PKG_MANAGER install -y"
+        CERT_INSTALL="sudo yum install epel-release -y; sudo yum install certbot -y"
+        DOCKER_INSTALL="sudo yum install -y yum-utils; sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo; sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y"
+        ;;
+    *)
+        echo "Unsupported OS: $OS"
+        exit 1
+        ;;
+esac
+
 # 1. Clone the repo
 echo "127.0.0.1 $(hostname)" | sudo tee -a /etc/hosts
-sudo apt update
-mkdir /var/app
+sudo $PKG_UPDATE
+mkdir -p /var/app
 cd /var/app
-sudo apt install git -y
+sudo $PKG_INSTALL git
 git clone -b dev https://github.com/khvilon/unkaos.git
 cd /var/app/unkaos
 
@@ -32,9 +77,7 @@ while IFS= read -r line
 do
     # Check if the line contains a key-value pair
     if [[ $line == *=* ]]; then
-
         if [[ $prev_line == "#>>"* ]]; then
-
             key=$(echo $line | cut -d'=' -f1)
             value=$(echo $line | cut -d'=' -f2)
       
@@ -64,26 +107,6 @@ mv $TEMP_FILE $ENV_FILE
 echo "Updated .env file saved."
 
 # 3. Make a copy of the server/db/public.sql with the changed schema
-
-# Function to validate the workspace name
-validate_workspace_name() {
-    local name=$1
-    if [[ -z $name ]]; then
-        echo "Workspace name cannot be empty."
-        return 1
-    elif [[ $name =~ [^a-zA-Z0-9_] ]]; then
-        echo "Workspace name can only contain letters, numbers, and underscores."
-        return 1
-    elif [[ ${#name} -ge 64 ]]; then
-        echo "Workspace name should be shorter than 64 characters."
-        return 1
-    elif [[ ! $name =~ ^[a-zA-Z_] ]]; then
-        echo "Workspace name should start with a letter or an underscore."
-        return 1
-    fi
-    return 0
-}
-
 while true; do
     read -p "Enter your first workspace name: " schema_name < /dev/tty
     validate_workspace_name "$schema_name"
@@ -100,12 +123,10 @@ sed -i "s/\btest\b/$schema_name/g" server/db/-workspace.sql
 random_password=$(generate_password)
 sed -i "s/mypass/$random_password/g" server/db/-workspace.sql
 
-#migrations
+# migrations
 for file in server/db/*_m.sql; do
     new_file="${file/_m/_m_$schema_name}"
-
     cp "$file" "$new_file"
-
     sed -i "s/\bpublic\b/$schema_name/g" "$new_file"
 done
 
@@ -114,28 +135,22 @@ read -p "Do you want to use Certbot for SSL setup? (yes by default, press ENTER)
 use_certbot=${use_certbot:-yes}  # Set default value
 if [[ $use_certbot != "no" ]]; then
     # Install Certbot
-    sudo apt install certbot -y
-
+    eval $CERT_INSTALL
     echo "try install cert 0"
-
     # Generate certificates
     source $ENV_FILE
     sudo certbot certonly --standalone -d $DOMAIN --register-unsafely-without-email --agree-tos --no-eff-email
-
     echo "try install cert 1"
-
     cp -rfL /etc/letsencrypt/live/$DOMAIN/* /var/app/unkaos/nginx/ssl
-
     # Create a Cron Job for auto-renewal of certificates
     (crontab -l ; echo "0 */12 * * * /usr/bin/certbot renew --quiet --post-hook \"docker-compose -f /var/app/unkaos/docker-compose.yml restart nginx\"") | crontab -
 fi
 
 # 5. Run docker containers
-apt install docker-compose -y
+eval $DOCKER_INSTALL
+sudo systemctl start docker
+sudo systemctl enable docker
 docker-compose up -d
-
-#apt install postgresql-client-common
-sudo apt-get install postgresql-client
 
 # 6. Set up a Cron Job to run update.sh every 5 minutes
 (crontab -l ; echo "*/5 * * * * /bin/bash /var/app/unkaos/update.sh") | crontab -
