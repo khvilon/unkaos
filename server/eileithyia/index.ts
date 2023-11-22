9af42aa1bca217bbbe4ef754bf6ba42490460316
Khvilon Nikolay <n@khvilon.ru>
2023-11-22 12:09:14 +0300

    CORS fix

--- server/eileithyia/index.ts
+++ server/eileithyia/index.ts
@@ -12,10 +12,13 @@ import fs from 'fs/promises';
 const app:any = express();
 const port = 5001;
 
-app.use(cors());
+
 app.use(express.json({limit: '1mb'}));
 app.use(express.raw({limit: '1mb'}));
 app.use(express.urlencoded({limit: '1mb', extended: true}));
+
+
+app.use(cors({origin: '*'}));
 
 let readWorkspaceRequest = async function(uuid: String){
     let ans = await sql`SELECT 
