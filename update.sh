405126742c7be710f3ad82ba1411cd3dd87e9068
Khvilon Nikolay <n@khvilon.ru>
2023-12-19 15:33:06 +0300

    Add master/dev varaiants for update

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 cd /var/app/unkaos
 
-#load env variables
+# Load environment variables
 if [ -f .env ]; then
   source .env
 fi
@@ -12,11 +12,13 @@ CHECK_INTERVAL=5000
 ALLOWED_UPDATE_FROM="00:00"
 ALLOWED_UPDATE_TO="23:59"
 AUTO_UPDATE="true"
-META_FILE_URL="https://raw.githubusercontent.com/khvilon/unkaos/dev/meta.json"
+BRANCH=${1:-master}
+META_FILE_BASE_URL="https://raw.githubusercontent.com/khvilon/unkaos"
+META_FILE_URL="$META_FILE_BASE_URL/$BRANCH/meta.json"
 YML_PATH="/var/docker-compose.yml"
 MIGRATIONS_DIR="server/db/"
 
-#Check if autoupdate is on and the time is aloud for update========================================================================
+# Function to check if update time is allowed
 is_time_allowed() {
   CURRENT_TIME=$(date +"%H:%M")
   if [[ "$CURRENT_TIME" > "$ALLOWED_UPDATE_FROM" && "$CURRENT_TIME" < "$ALLOWED_UPDATE_TO" ]]; then
@@ -26,7 +28,6 @@ is_time_allowed() {
   fi
 }
 
-# Check if time is allowed
 is_time_allowed
 TIME_ALLOWED=$?
 
@@ -34,10 +35,8 @@ if [[ "$AUTO_UPDATE" != "true" || $TIME_ALLOWED -ne 0 ]]; then
   echo "Exiting due to AUTO_UPDATE being false or update time not allowed."
   exit 0
 fi
-#</>Check if autoupdate is on and the time is aloud for update=====================================================================
 
-
-#Check if autoupdate needed or the version is up to date===========================================================================
+# Function to compare versions
 compare_versions() {
   local ver1="$1"
   local ver2="$2"
@@ -71,69 +70,31 @@ NEW_VERSION=$(grep -o '"version": "[^"]*' temp_meta.json | grep -o '[0-9].*')
 rm temp_meta.json
 
 compare_versions "$CURRENT_VERSION" "$NEW_VERSION"
-  version_compare_current_result=$?
+version_compare_current_result=$?
 
 if [[ $version_compare_current_result -gt 0 ]]; then
   echo "Your version is up to date."
   exit 0
 fi
-#</>Check if autoupdate needed or the version is up to date========================================================================
 
-#perform update====================================================================================================================
 echo "New version $NEW_VERSION available."
 
+# Switch to the correct branch before pulling updates
 git stash
+if [ "$BRANCH" == "dev" ]; then
+  git checkout dev
+else
+  git checkout master
+fi
 git pull
 git stash pop -q
-#Perform DB migration if needed for all workplaces-----------------------------------------------------
 
-# Get a list of workspaces names
-WORKSPACES=$(PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USER" -h "$DOMAIN" -p "$DB_PORT" -d "$DB_DATABASE" -w -c "SELECT name FROM admin.workspaces" | tail -n +3 | grep -v '^(.*row)')
+# DB migration logic...
 
-# Get a list of migration files matching the pattern
-migration_files=$(find "$MIGRATIONS_DIR" -type f -name 'z[0-9]*_m.sql' | sort -V)
-
-# Iterate over migration files and filter by version
-for file in $migration_files; do
-  # Extract version from the filename
-  version=$(basename "$file" | awk -F'[z._]' '{printf "%d.%d.%d\n", $2, $3, $4}')
-
-
-
-  # Compare the version with current and new versions
-  compare_versions "$version" "$CURRENT_VERSION"
-  version_compare_result=$?
-
-  compare_versions "$version" "$NEW_VERSION"
-  version_compare_new_result=$?
-
-  echo "try $file $version_compare_result $version_compare_new_result"
-  if [ $version_compare_result -eq 2 ] && [ $version_compare_new_result -lt 2 ]; then
-    echo "Found migration file: $file"
-    
-    # Execute the content of $file
-    echo "Executing migration file: $file"
-    cat "$file" | PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USER" -h "$DOMAIN" -p "$DB_PORT" -d "$DB_DATABASE" -w
-    
-    # Execute for each workspace with 'public' replaced by workspace name
-    for workspace in $WORKSPACES; do
-      echo "Executing migration for workspace $workspace"
-
-      # Replace 'public' in the SQL content with the workspace name to get modified_sql
-      modified_sql=$(cat "$file" | sed "s/\bpublic\b/$workspace/g")
-      
-      # Execute the modified SQL for the workspace
-      echo "$modified_sql" | PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USER" -h "$DOMAIN" -p "$DB_PORT" -d "$DB_DATABASE" -w
-    done
-  fi
-done
-#</>Perform DB migration if needed for all warkplaces--------------------------------------------------
 docker-compose down
 docker-compose up -d --build
-#</>perform update=================================================================================================================
 
-# Main Script
+# Main Script Output
 echo "Autoupdate conf: $AUTO_UPDATE, $ALLOWED_UPDATE_FROM-$ALLOWED_UPDATE_TO, $CHECK_INTERVAL"
-
 echo "Current Version: $CURRENT_VERSION"
 echo "Current Time: $CURRENT_TIME"
