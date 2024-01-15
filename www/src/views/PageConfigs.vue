d794b313022c8e722dbffc3e9312905a327cd5b0
Khvilon Nikolay <n@khvilon.ru>
2024-01-15 12:27:59 +0300

    Add server_config

--- www/src/views/PageConfigs.vue
+++ www/src/views/PageConfigs.vue
@@ -6,7 +6,43 @@ import page_helper from "../page_helper.ts";
   <div>
     <TopMenu :label="'Настройки'" />
     <div id="config_down_panel" class="panel">
-      <span>Страница в разработке</span>
+      <KTabPanel>
+        <KTab title="Основное">
+        
+
+        </KTab>
+        <KTab title="Оповещения">
+          <string-input
+            label="Почта - сервис"
+          />  
+          <string-input
+            label="Почта - пользователь"
+          />
+          <string-input
+            label="Почта - пароль"
+          />  
+          <string-input
+            label='Почта - поле "ОТ"'
+          />  
+          <string-input
+            label='Discord - токен бота'
+          />
+          <string-input
+            label='Telegram - токен бота'
+          />  
+        </KTab>
+        <KTab title="Автообновление">
+          
+        </KTab>
+        <KTab title="ИИ">
+          <string-input
+            label="Ключ openai API"
+          />  
+          <numeric-input
+            label="Допустимое количество запросов на рабочее пространство в день"
+          />  
+        </KTab>
+      </KTabPanel>
     </div>
   </div>
 </template>
@@ -21,6 +57,7 @@ $card-width: 400px;
 #config_card {
   margin: 1px;
   height: calc(100vh - $top-menu-height);
+
 }
 
 #config_table_panel {
@@ -49,6 +86,13 @@ $card-width: 400px;
 #config_down_panel {
   display: flex;
   height: calc(100vh - $top-menu-height);
+  background: transparent;
+  padding: 20px;
+}
+
+#config_down_panel .tab-panel{
+  width: 100%;
+  height: 100%;
 }
 
 #config_down_panel span {
