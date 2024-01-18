018c4f422d08f0048c1951d779d26e067305fb50
Khvilon Nikolay <n@khvilon.ru>
2024-01-19 02:12:14 +0300

    Use db configs for autoupdate

--- update.sh
+++ update.sh
@@ -6,6 +6,8 @@ OS_ID="${OS_ID//\"/}" # Remove quotes from the string
 
 cd /var/app/unkaos
 
+CONFIGS=$(PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USER" -h "$DOMAIN" -p "$DB_PORT" -d "$DB_DATABASE" -w -c "SELECT name FROM admin.workspaces" | tail -n +3 | grep -v '^(.*row)')
+
 # Load environment variables
 if [ -f .env ]; then
   source .env
@@ -13,8 +15,8 @@ fi
 
 # Configuration
 CHECK_INTERVAL=5000
-ALLOWED_UPDATE_FROM="00:00"
-ALLOWED_UPDATE_TO="23:59"
+ALLOWED_UPDATE_FROM=0
+ALLOWED_UPDATE_TO=23
 AUTO_UPDATE="true"
 BRANCH=${1:-master}
 META_FILE_BASE_URL="https://raw.githubusercontent.com/khvilon/unkaos"
@@ -22,15 +24,57 @@ META_FILE_URL="$META_FILE_BASE_URL/$BRANCH/meta.json"
 YML_PATH="/var/docker-compose.yml"
 MIGRATIONS_DIR="server/db/"
 
+
+# Fetch configurations
+CONFIGS=$(PGPASSWORD="$DB_PASSWORD" psql -U "$DB_USER" -h "$DOMAIN" -p "$DB_PORT" -d "$DB_DATABASE" -w -c "SELECT name, value FROM server.configs WHERE service = 'autoupdate'" | sed -n '/^ /p' | tr -s ' ' | cut -d ' ' -f2,3)
+
+# Process and assign values to variables
+while IFS= read -r line; do
+    name=$(echo "$line" | cut -d ' ' -f1)
+    value=$(echo "$line" | cut -d ' ' -f2)
+
+    case $name in
+        "from")
+            ALLOWED_UPDATE_FROM=$value
+            ;;
+        "to")
+            ALLOWED_UPDATE_TO=$value
+            ;;
+        "allow")
+            AUTO_UPDATE=$value
+            ;;
+    esac
+done <<< "$CONFIGS"
+
+# Echo variables for verification
+echo "ALLOWED_UPDATE_FROM: $ALLOWED_UPDATE_FROM"
+echo "ALLOWED_UPDATE_TO: $ALLOWED_UPDATE_TO"
+echo "AUTO_UPDATE: $AUTO_UPDATE"
+
 # Function to check if update time is allowed
 is_time_allowed() {
-  CURRENT_TIME=$(date +"%H:%M")
-  if [[ "$CURRENT_TIME" > "$ALLOWED_UPDATE_FROM" && "$CURRENT_TIME" < "$ALLOWED_UPDATE_TO" ]]; then
-    return 0
+  # Get the current hour
+  CURRENT_HOUR=$(date +"%H")
+
+  # Convert the current hour to an integer
+  CURRENT_HOUR=$(printf "%d" "$CURRENT_HOUR")
+
+  # Check if update is allowed based on the time range
+  if [[ "$ALLOWED_UPDATE_FROM" -le "$ALLOWED_UPDATE_TO" ]]; then
+    # Time range does not span midnight
+    if [[ "$CURRENT_HOUR" -ge "$ALLOWED_UPDATE_FROM" && "$CURRENT_HOUR" -lt "$ALLOWED_UPDATE_TO" ]]; then
+      return 0
+    fi
   else
-    return 1
+    # Time range spans midnight
+    if [[ "$CURRENT_HOUR" -ge "$ALLOWED_UPDATE_FROM" || "$CURRENT_HOUR" -lt "$ALLOWED_UPDATE_TO" ]]; then
+      return 0
+    fi
   fi
+
+  return 1
 }
+
 
 is_time_allowed
 TIME_ALLOWED=$?
