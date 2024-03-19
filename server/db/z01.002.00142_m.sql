6d1b40d31c828de1da33e33e007570c5060e198c
Khvilon Nikolay <n@khvilon.ru>
2024-03-19 03:44:12 +0300

    fix setup sql

--- server/db/z01.002.00142_m.sql
+++ server/db/z01.002.00142_m.sql
@@ -1,2 +1,3 @@
 INSERT INTO server.gadget_types (uuid, name, code) VALUES 
-('bfa1e4bf-f635-4eeb-89e9-c9c04c39b366', 'График сгорания?', 'TimeReport');
+('bfa1e4bf-f635-4eeb-89e9-c9c04c39b366', 'График сгорания?', 'TimeReport')
+ON CONFLICT (uuid) DO NOTHING;
