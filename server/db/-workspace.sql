0297b853ae224e1afba60038548a3667efd0a56f
Khvilon Nikolay <n@khvilon.ru>
2024-05-19 02:22:25 +0300

    Fix gadgets common permission

--- server/db/-workspace.sql
+++ server/db/-workspace.sql
@@ -215,6 +215,7 @@ INSERT INTO server.permissions (uuid, code, name, targets) VALUES
     {"allow": "R", "table": "dashboard"},
     {"allow": "R", "table": "board"},
     {"allow": "R", "table": "boards_filters"},
+    {"allow": "R", "table": "gadgets"},
 
     {"allow": "R", "table": "field_types"},
     {"allow": "R", "table": "gadget_types"},
