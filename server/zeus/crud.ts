2a2af49ba10ee7b119448ad6d492d71c1a920e27
Khvilon Nikolay <n@khvilon.ru>
2024-05-19 01:58:56 +0300

    Fix zeus crud filter + issue styles fixes

--- server/zeus/crud.ts
+++ server/zeus/crud.ts
@@ -1043,18 +1043,19 @@ crud.updateQueryWithProjectsPermissionsFilter = async function(subdomain: string
     
     if(method == "read"){
       let uuids_str = `('${projects_uuids_array.join("','")}')`;
-      if(table_name == "projects" || table_name == "watcher" || table_name == "attachments" || 
-      table_name == "time_entries" || table_name == "issue_tags_selected" || table_name == "issue_actions")
-        query = query.replace('t1.deleted_at IS NULL', 't1.deleted_at IS NULL AND t1.uuid IN ' + uuids_str)
+      if(table_name == "projects") query = query.replace('t1.deleted_at IS NULL', 't1.deleted_at IS NULL AND t1.uuid IN ' + uuids_str)
+     /* if(table_name == "watcher" || table_name == "attachments" || table_name == "time_entries" || 
+      table_name == "issue_tags_selected" || table_name == "issue_actions" || table_name == "issue_formated_actions")
+        query = query.replace('t1.deleted_at IS NULL', 't1.deleted_at IS NULL AND t1.uuid IN ' + uuids_str)*/
+      //TODO - filter correctly
       else if(table_name == "issues") query = query.replace('T1.DELETED_AT IS NULL', 'T1.DELETED_AT IS NULL AND T1.PROJECT_UUID IN ' + uuids_str)
       else if(table_name == "issues_count") query += ' AND PROJECT_UUID IN ' + uuids_str
       else if(table_name == "short_issue_info") query = query.replace('I.DELETED_AT IS NULL', 'I.DELETED_AT IS NULL AND I.PROJECT_UUID IN ' + uuids_str)
       else if(table_name == "short_issue_info_for_imort") query = query.replace('I.DELETED_AT IS NULL', 'I.DELETED_AT IS NULL AND I.PROJECT_UUID IN ' + uuids_str)
-      //TODO issue_actions issue_formated_actions
     }
     else{
       if(table_name == "projects" && !projects_uuids_array.includes(params.uuid)) return false;
-      //todo - add filtering projects
+      //TODO - add filtering projects
       if(table_name == "issues"  && !projects_uuids_array.includes(readed_data.rows[0].project_uuid)) return false;
     }
 
