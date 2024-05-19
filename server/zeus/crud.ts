e170753bd9a3f80c67200d97bb13fc11e84444a3
Khvilon Nikolay <n@khvilon.ru>
2024-05-19 04:17:17 +0300

    Fix fields save

--- server/zeus/crud.ts
+++ server/zeus/crud.ts
@@ -1197,7 +1197,7 @@ crud.do = async function (subdomain:string, method:string, table_name:string, pa
       }
 
       if (del_query != ""){ //&& table_name != 'users') {
-        let [q, p] = crud.push_query([query, pg_params, del_query], [[]], false);
+        let [q, p] = crud.push_query(query, pg_params, del_query, [[]], true);
 
         query = q;
         pg_params = p;
